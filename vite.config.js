0bab5b115f8c0be8c31d0ba3503fa5169a0744b5
vkatti1109 <vkatti1109@users.noreply.github.com>
2025-06-20 12:37:58 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
-import { defineConfig } from 'vite' import react from '@vitejs/plugin-react' import path from 'path' // https://vitejs.dev/config/ export default defineConfig({ plugins: [react( )], resolve: { alias: { "@": path.resolve(__dirname, "./src"), }, }, build: { outDir: 'dist', assetsDir: 'assets', sourcemap: false, minify: 'terser', rollupOptions: { output: { manualChunks: { vendor: ['react', 'react-dom'], router: ['react-router-dom'] } } } }, server: { port: 3000, host: true } })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+});
